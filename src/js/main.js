085bca5697dcf6e3b40f4003b66bfb0d508dce62
Vladimir Lachugin <lachuginvv@icloud.com>
2022-06-04 12:16:06 +0300

    slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,10 @@
-const sum = require('./module/sum');
+import $ from "jquery";
+import "slick-carousel";
 
-console.log(sum(2, 5));
-console.log(sum(5, 5));
+
+$(document).ready(function(){
+  $('.slider__list').slick({
+    prevArrow: `<button type="button" class="slick-prev"><img src="img/chevron-left-solid.png" alt="1"></button>`,
+    nextArrow: `<button type="button" class="slick-next"><img src="img/chevron-right-solid.png" alt="2"></button>`,
+  });
+});
